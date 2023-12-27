84d4305f96b6e2f884f7cf6908ef300872630d48
Yann <yann.hobbe@supinfo.com>
2023-12-27 23:03:44 +0100

    welcome animation

--- src/GUI/lobbyGUI.py
+++ src/GUI/lobbyGUI.py
@@ -4,6 +4,29 @@ from pygame import mixer
 from ttkthemes import ThemedTk
 from gameGUI import GameGUI
 
+class WelcomeTextAnimator:
+    def __init__(self, root, label):
+        self.root = root
+        self.label = label
+
+        self.text = "Welcome !"
+        self.x_position = root.winfo_screenwidth() // 2
+        self.speed = 100  # ~~~> temps
+
+        self.text_already_written = ""
+        self.text_to_write = self.text
+
+    def animate_text(self):
+        self.label.config(text=self.text_already_written)
+
+        if self.text_to_write:
+            current_letter = self.text_to_write[:1]
+            self.text_already_written += current_letter
+            self.text_to_write = self.text_to_write[1:]
+
+        if self.text_to_write:
+            self.root.after(self.speed, self.animate_text)
+
 class LobbyGUI:
     def __init__(self):
         self.initialize_window()
@@ -14,12 +37,43 @@ class LobbyGUI:
         main_frame = ttk.Frame(self.root, padding=500)
         main_frame.place(relx=0.5, rely=0.5, anchor='center')
 
-        # self.create_welcome_label(main_frame)
-        self.create_size_controls(main_frame)
-        self.create_pawn_controls(main_frame)
-        self.create_quit_button(main_frame)
-        self.create_volume_control(main_frame)
-        self.create_start_game_button(main_frame)
+        # Create a label for welcome text
+        welcome_label = ttk.Label(main_frame, text="Welcome to the Game Lobby", font=("Helvetica", 40))
+        welcome_label.grid(row=0, column=0, columnspan=2, pady=(0, 100))
+
+        # Initialize the text animator
+        self.welcome_text_animator = WelcomeTextAnimator(self.root, welcome_label)
+
+        # Size controls
+        label_font_size = int(24 * self.root.winfo_width() / 1920)
+        ttk.Label(main_frame, text="Choose the size of the game board", anchor='center', font=("TkDefaultFont", label_font_size)).grid(columnspan=3, pady=(0, 100))
+        size_slider = ttk.Scale(main_frame, from_=8, to=12, variable=self.size_var, orient=tk.HORIZONTAL, length=self.root.winfo_screenwidth() // 2, command=self.update_size_slider_label, style="TScale")
+        size_slider.grid(row=1, columnspan=2, pady=(20, 10))
+        self.create_position_canvases(main_frame, [8, 9, 10, 11, 12], [0.5, 12.75, 25, 37.2, 49.5], 260, 'center', self.size_canvases)
+        self.size_label = ttk.Label(main_frame, text=f"Size: {int(self.size_var.get())}x{int(self.size_var.get())}", anchor='center', font=("TkDefaultFont", label_font_size))
+        self.size_label.grid(row=3, column=0, columnspan=2, pady=(10, 40))
+
+        # Pawn controls
+        ttk.Label(main_frame, text="Choose pawn", anchor='center', font=("TkDefaultFont", label_font_size)).grid(row=4, column=0, columnspan=3, pady=(10, 10))
+        pawn_slider = ttk.Scale(main_frame, from_=4, to=6, variable=self.pawn_var, orient=tk.HORIZONTAL, length=self.root.winfo_screenwidth() // 4, command=self.update_pawn_slider_label, style="TScale")
+        pawn_slider.grid(row=5, column=0, columnspan=2, pady=(20, 10))
+        self.create_position_canvases(main_frame, [4, 5, 6], [13, 25, 37], 475, 'center', self.pawn_canvases)
+        self.pawn_label = ttk.Label(main_frame, text=f"Chosen Pawn: {int(self.pawn_var.get())}", anchor='center', font=("TkDefaultFont", label_font_size))
+        self.pawn_label.grid(row=6, column=0, columnspan=2, pady=(40, 40))
+
+        # Buttons
+        quit_button_text = "Go windowed"
+        quit_button = ttk.Button(main_frame, text=quit_button_text, command=self.toggle_fullscreen)
+        quit_button.grid(row=7, column=0, columnspan=2, pady=(10, 10))
+        new_window_button = ttk.Button(main_frame, text="Start Game", command=self.open_new_window)
+        new_window_button.grid(row=8, column=0, columnspan=2, pady=(20, 0))
+
+        # Volume control
+        volume_slider = ttk.Scale(main_frame, from_=0, to=100, variable=self.volume_var, orient=tk.HORIZONTAL, length=self.root.winfo_screenwidth() // 4, command=self.update_volume, style="TScale")
+        volume_slider.grid(row=9, column=0, columnspan=2, pady=(20, 0), padx=(0, 20), sticky='se')
+
+        # Start the text animation
+        self.welcome_text_animator.animate_text()
 
     def initialize_window(self):
         self.root = ThemedTk(theme="black")
@@ -40,44 +94,6 @@ class LobbyGUI:
         self.volume_var = tk.DoubleVar(value=50)
         self.size_canvases = []
         self.pawn_canvases = []
-
-    def create_welcome_label(self, frame):
-        welcome_label = tk.Label(frame, text="Welcome to the Game Lobby", font=("Helvetica", 16))
-        welcome_label.grid(row=0, column=0, pady=(0, 100), padx=(100, 0))
-
-    def create_size_controls(self, frame):
-        label_font_size = int(24 * self.root.winfo_width() / 1920)
-        ttk.Label(frame, text="Choose the size of the game board", anchor='center', font=("TkDefaultFont", label_font_size)).grid(columnspan=3, pady=(0, 10))
-
-        size_slider = ttk.Scale(frame, from_=8, to=12, variable=self.size_var, orient=tk.HORIZONTAL, length=self.root.winfo_screenwidth() // 2, command=self.update_size_slider_label, style="TScale")
-        size_slider.grid(row=1, columnspan=2, pady=(20, 10))
-
-        self.create_position_canvases(frame, [8, 9, 10, 11, 12], [0.5, 12.75, 25, 37.2, 49.5], 85, 'center', self.size_canvases)
-        self.size_label = ttk.Label(frame, text=f"Size: {int(self.size_var.get())}x{int(self.size_var.get())}", anchor='center', font=("TkDefaultFont", label_font_size))
-        self.size_label.grid(row=3, column=0, columnspan=2, pady=(40, 40))
-
-    def create_pawn_controls(self, frame):
-        label_font_size = int(24 * self.root.winfo_width() / 1920)
-        ttk.Label(frame, text="Choose pawn", anchor='center', font=("TkDefaultFont", label_font_size)).grid(row=4, column=0, columnspan=3, pady=(10, 10))
-        pawn_slider = ttk.Scale(frame, from_=4, to=6, variable=self.pawn_var, orient=tk.HORIZONTAL, length=self.root.winfo_screenwidth() // 4, command=self.update_pawn_slider_label, style="TScale")
-        pawn_slider.grid(row=5, column=0, columnspan=2, pady=(20, 10))
-
-        self.create_position_canvases(frame, [4, 5, 6], [13, 25, 37], 290, 'center', self.pawn_canvases)
-        self.pawn_label = ttk.Label(frame, text=f"Chosen Pawn: {int(self.pawn_var.get())}", anchor='center', font=("TkDefaultFont", label_font_size))
-        self.pawn_label.grid(row=6, column=0, columnspan=2, pady=(40, 40))
-
-    def create_quit_button(self, frame):
-        quit_button_text = "Go windowed"
-        self.quit_button = ttk.Button(frame, text=quit_button_text, command=self.toggle_fullscreen)
-        self.quit_button.grid(row=7, column=0, columnspan=2, pady=(10, 10))
-        
-    def create_start_game_button(self, frame):
-        new_window_button = ttk.Button(frame, text="Start Game", command=self.open_new_window)
-        new_window_button.grid(row=8, column=0, columnspan=2, pady=(20, 0))
-
-    def create_volume_control(self, frame):
-        volume_slider = ttk.Scale(frame, from_=0, to=100, variable=self.volume_var, orient=tk.HORIZONTAL, length=self.root.winfo_screenwidth() // 4, command=self.update_volume, style="TScale")
-        volume_slider.grid(row=9, column=0, columnspan=2, pady=(20, 0), padx=(0, 20), sticky='se')
 
     def create_position_canvases(self, frame, values, positions, y_position, anchor, canvases):
         screen_width = self.root.winfo_screenwidth()
@@ -106,14 +122,14 @@ class LobbyGUI:
         if self.root.attributes('-fullscreen'):
             self.root.attributes('-fullscreen', False)
             self.quit_button["text"] = "Go fullscreen"
-
-            # Center Window
             x = (self.root.winfo_screenwidth() - self.root.winfo_width()) // 2
             y = (self.root.winfo_screenheight() - self.root.winfo_height()) // 2
             self.root.geometry(f"{self.root.winfo_width()}x{self.root.winfo_height()}+{x}+{y}")
         else:
             self.root.attributes('-fullscreen', True)
             self.quit_button["text"] = "Go windowed"
+
+
 
     def open_new_window(self):
         self.root.destroy()
