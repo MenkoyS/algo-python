d4442e08f74bbf0a67c52141823e91be8c7d5dc9
Yann <yann.hobbe@supinfo.com>
2023-12-28 17:10:52 +0100

    je fais une oause

--- src/game_logic/gameGUI.py
+++ src/game_logic/gameGUI.py
@@ -1,41 +1,74 @@
 import tkinter as tk
+from tkinter import *
 from game import Game
 
 class GameGUI:
-    """
-    Initialize the graphical user interface for the game board.
-    :param game: Game, An instance of the Game class representing the game logic.
-    """
-
     def __init__(self, game):
         self.game = game
         self.window = tk.Tk()
         self.window.title("GameGUI")
+
+        # Set fullscreen
+        self.window.attributes('-fullscreen', True)
+
+        # Create a frame for the game board
+        self.board_frame = tk.Frame(self.window)
+        self.board_frame.grid(row=0, column=0, sticky="nsew")
+
+        # Create a frame for the buttons, centered vertically
+        self.button_frame = tk.Frame(self.window)
+        self.button_frame.grid(row=0, column=1, sticky="nsew")
+        self.button_frame.columnconfigure(0, weight=1)  # Ensure center alignment
+
+        # Create a spacer frame to center the buttons vertically
+        spacer_frame = tk.Frame(self.button_frame)
+        spacer_frame.pack(expand=True, fill=tk.Y)
+
+        # Configure rows and columns to expand for both frames
+        self.window.rowconfigure(0, weight=1)
+        self.window.columnconfigure(0, weight=1)
+        self.window.columnconfigure(1, weight=1)
+
+        # Configure rows and columns to expand for the game board frame
+        for i in range(self.game.getRows()):
+            self.board_frame.rowconfigure(i, weight=1)
+        for j in range(self.game.getColumns()):
+            self.board_frame.columnconfigure(j, weight=1)
+
         self.round = -1
         self.playerToPlay = self.game.getPlayer1()
 
         # Create a 2D list to store references to the labels
         self.labels = [[None for _ in range(self.game.getColumns())] for _ in range(self.game.getRows())]
 
-        # Create the game board labels
+        # Create the game board labels with spacing
         for row in range(self.game.getRows()):
             for col in range(self.game.getColumns()):
-                label = tk.Label(self.window, text="", width=5, height=2,
-                                 relief=tk.RIDGE, borderwidth=1, bg="black", fg="white")
-                label.grid(row=row, column=col)
+                label = tk.Label(self.board_frame, text="", width=5, height=2,
+                                 relief=tk.RIDGE, borderwidth=1, bg="black", fg="white", font=("Helvetica", 16),
+                                 padx=5, pady=5)  # Add padx and pady for spacing
+                label.grid(row=row, column=col, sticky="nsew")
                 label.bind("<Button-1>", lambda event, r=row,
                            c=col: self.on_label_click(r, c))
                 self.labels[row][col] = label
+
+        # Create buttons in the spacer frame for vertical centering
+        btn_restart = tk.Button(spacer_frame, text="Restart", command=self.restart_game)
+        btn_restart.pack(side=tk.TOP, pady=10)
+
+        btn_quit = tk.Button(spacer_frame, text="Quit", command=self.window.destroy)
+        btn_quit.pack(side=tk.TOP, pady=10)
+
+        btn_save = tk.Button(spacer_frame, text="Save", command=self.save)
+        btn_save.pack(side=tk.TOP, pady=10)
+
+        btn_go_window = tk.Button(spacer_frame, text="Go window", command=self.window)
+        btn_go_window.pack(side=tk.TOP, pady=10)
 
         # Update the board after creating clickable labels
         self.update_board()
 
     def on_label_click(self, row, col):
-        """
-        Handle the click event on a label.
-        :param row: int, The row index of the clicked label.
-        :param col: int, The column index of the clicked label.
-        """
         # Handle clicks based on the game state
         if self.round == -1:
             # Place player 1 pawn
@@ -73,27 +106,29 @@ class GameGUI:
                 # If the player has won
                 if self.game.checkWin(self.playerToPlay, self.game.pawnsToAlign):
                     winner = self.playerToPlay.getPlayer()
-                    print(f"Player {winner} won !")
+                    print(f"Player {winner} won!")
                     return winner
 
                 # Inverse player turn
                 self.playerToPlay = self.game.getPlayer2() if self.playerToPlay == self.game.getPlayer1() else self.game.getPlayer1()
 
     def update_board(self):
-        """
-        Update the text on the labels based on the game state.
-        """
-        # Update the text on the labels based on the game state
-        for row in range(self.game.rows):
-            for col in range(self.game.columns):
+        for row in range(self.game.getRows()):
+            for col in range(self.game.getColumns()):
                 cell_value = self.game.getCell(row, col)
-                text = "B" if cell_value == 1 else "R" if cell_value == 2 else "1" if cell_value == self.game.player1 else "2" if cell_value == self.game.player2 else ""
+                text = "R" if cell_value == 1 else "B" if cell_value == 2 else "1" if cell_value == self.game.player1 else "2" if cell_value == self.game.player2 else ""
                 self.labels[row][col].config(text=text)
 
+    def restart_game(self):
+        pass
+
+    def save(self):
+        pass
+        
+    def window(self):
+        pass
+
     def run(self):
-        """
-        Run the Tkinter main loop to display the game GUI.
-        """
         self.window.mainloop()
 
 if __name__ == "__main__":
